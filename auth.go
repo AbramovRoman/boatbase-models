fcf764317d078f50b7ff195ca57a983207616d1b
AbramovRoman <romanadramov56@gmail.com>
2026-05-28 10:42:26 +0400

    add Role to Session

--- auth.go
+++ auth.go
@@ -9,4 +9,5 @@ type Session struct {
 	Token         string `json:"token"`           //Уникальный токен
 	StateNumber   string `json:"state_number"`    //госномер
 	OwnerFullName string `json:"owner_full_name"` //Имя
+	Role          string `json:"role"`
 }
